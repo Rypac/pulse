898f19c2f9ea58862218f0ed427659701cf47b1f
Ryan Davis <ryan.davis.dev@gmail.com>
2016-03-06 10:37:34 +1100

    Add meta library required by ranges

--- install_libs.sh
+++ install_libs.sh
@@ -8,3 +8,4 @@ test -d $INSTALL_DIR || mkdir -p $INSTALL_DIR
 cp -r $LIB_DIR/rdl/include/rdl/ $INSTALL_DIR/rdl/
 cp -r $LIB_DIR/gsl/include/ $INSTALL_DIR/gsl/
 cp -r $LIB_DIR/range-v3/include/range/ $INSTALL_DIR/range/
+cp -r $LIB_DIR/meta/include/meta/ $INSTALL_DIR/meta/
