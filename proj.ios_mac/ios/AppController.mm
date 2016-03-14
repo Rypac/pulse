030909d13844f50f7cb0ceade51fa3253b210c9e
Ryan Davis <ryan.davis.dev@gmail.com>
2016-03-14 13:14:12 +1100

    Refactor AppController Objective-C file

--- proj.ios_mac/ios/AppController.mm
+++ proj.ios_mac/ios/AppController.mm
@@ -9,31 +9,30 @@
 static AppDelegate s_sharedApplication;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    auto app = cocos2d::Application::getInstance();
+    cocos2d::Application* app = cocos2d::Application::getInstance();
     app->initGLContextAttrs();
     cocos2d::GLViewImpl::convertAttrs();
 
-    window = [[UIWindow alloc] initWithFrame: [[UIScreen mainScreen] bounds]];
+    window = [[UIWindow alloc] initWithFrame: [UIScreen mainScreen].bounds];
 
-    auto eaglView = [CCEAGLView viewWithFrame:[window bounds]
-                                  pixelFormat:(NSString*)cocos2d::GLViewImpl::_pixelFormat
-                                  depthFormat:cocos2d::GLViewImpl::_depthFormat
-                           preserveBackbuffer:NO
-                                   sharegroup:nil
-                                multiSampling:NO
-                              numberOfSamples:0];
-    [eaglView setMultipleTouchEnabled:NO];
+    CCEAGLView* eaglView = [CCEAGLView viewWithFrame:window.bounds
+                                         pixelFormat:(NSString*)cocos2d::GLViewImpl::_pixelFormat
+                                         depthFormat:cocos2d::GLViewImpl::_depthFormat
+                                  preserveBackbuffer:NO
+                                          sharegroup:nil
+                                       multiSampling:NO
+                                     numberOfSamples:0];
+    eaglView.multipleTouchEnabled = NO;
 
     _viewController = [[RootViewController alloc] initWithNibName:nil bundle:nil];
-    _viewController.wantsFullScreenLayout = YES;
     _viewController.view = eaglView;
 
-    [window setRootViewController:_viewController];
+    window.rootViewController = _viewController;
     [window makeKeyAndVisible];
 
-    [[UIApplication sharedApplication] setStatusBarHidden:true];
+    [UIApplication sharedApplication].statusBarHidden = YES;
 
-    auto glview = cocos2d::GLViewImpl::createWithEAGLView(eaglView);
+    cocos2d::GLViewImpl* glview = cocos2d::GLViewImpl::createWithEAGLView(eaglView);
     cocos2d::Director::getInstance()->setOpenGLView(glview);
 
     app->run();
