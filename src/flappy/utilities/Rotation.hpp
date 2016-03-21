0b3aa737e8787b6e9500b84e00e2f48469d3da2b
Ryan Davis <ryan.davis.dev@gmail.com>
2016-03-21 13:11:40 +1100

    Flip calculation to stop inversion of resulting angle

--- src/flappy/utilities/Rotation.hpp
+++ src/flappy/utilities/Rotation.hpp
@@ -5,11 +5,11 @@
 namespace flappy { namespace rotation {
 
 inline double angle(cocos2d::Vec2 p2, cocos2d::Vec2 p1 = cocos2d::Vec2{1, 0}) {
-    return -asin((p1.x * p2.y - p2.x * p1.y) / (p1.x * p1.x + p1.y * p1.y + MATH_FLOAT_SMALL));
+    return asin((p1.x * p2.y - p2.x * p1.y) / (p1.x * p1.x + p1.y * p1.y + MATH_EPSILON));
 }
 
 inline double pitch(cocos2d::Vec3 p2, cocos2d::Vec3 p1) {
-    return angle(cocos2d::Vec2{p2.z, p2.y}, cocos2d::Vec2{p1.z, p1.y});
+    return angle(cocos2d::Vec2{p2.y, p2.z}, cocos2d::Vec2{p1.y, p1.z});
 }
 
 inline double yaw(cocos2d::Vec3 p2, cocos2d::Vec3 p1) {
