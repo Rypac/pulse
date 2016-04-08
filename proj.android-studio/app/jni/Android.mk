0c9f233b73a487048ce18d31c04a016f8d9da733
Ryan Davis <ryan.davis.dev@gmail.com>
2016-04-08 23:12:34 +1000

    Cleanup Android NDK makefile

--- proj.android-studio/app/jni/Android.mk
+++ proj.android-studio/app/jni/Android.mk
@@ -1,47 +1,43 @@
 LOCAL_PATH := $(call my-dir)
 ROOT_PATH := $(LOCAL_PATH)/../../..
-SRC_PATH := ../../../src/pulse
+COCOS_PATH := $(ROOT_PATH)/cocos2d
+LIB_PATH := $(ROOT_PATH)/libs
+INC_PATH := $(ROOT_PATH)/src
+SRC_PATH := ../../../src
+PULSE_PATH := $(SRC_PATH)/pulse
 
 include $(CLEAR_VARS)
 
-$(call import-add-path,$(ROOT_PATH)/cocos2d)
-$(call import-add-path,$(ROOT_PATH)/cocos2d/external)
-$(call import-add-path,$(ROOT_PATH)/cocos2d/cocos)
+$(call import-add-path,$(COCOS_PATH))
+$(call import-add-path,$(COCOS_PATH)/external)
+$(call import-add-path,$(COCOS_PATH)/cocos)
 
 LOCAL_MODULE := cocos2dcpp_shared
-
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
-LOCAL_SRC_FILES := bootstrap/main.cpp \
-                   $(SRC_PATH)/AppDelegate.cpp \
-                   $(SRC_PATH)/scenes/DeveloperMenuScene.cpp \
-                   $(SRC_PATH)/scenes/GameScene.cpp \
-                   $(SRC_PATH)/scenes/PulseGameScene.cpp \
-                   $(SRC_PATH)/scenes/SplashScene.cpp \
-                   $(SRC_PATH)/sprites/Column.cpp \
-                   $(SRC_PATH)/sprites/Pulse.cpp \
-                   $(SRC_PATH)/sprites/Obstacle.cpp \
-                   $(SRC_PATH)/sprites/SpritePhysicsBody.cpp \
-                   $(SRC_PATH)/sprites/ObstaclePhysicsBody.cpp \
-                   $(SRC_PATH)/ui/MenuSlider.cpp \
-                   $(SRC_PATH)/generators/ObstacleGenerator.cpp
+LOCAL_SRC_FILES := \
+    bootstrap/main.cpp \
+    $(PULSE_PATH)/AppDelegate.cpp \
+    $(PULSE_PATH)/scenes/DeveloperMenuScene.cpp \
+    $(PULSE_PATH)/scenes/GameScene.cpp \
+    $(PULSE_PATH)/scenes/PulseGameScene.cpp \
+    $(PULSE_PATH)/scenes/SplashScene.cpp \
+    $(PULSE_PATH)/sprites/Column.cpp \
+    $(PULSE_PATH)/sprites/Pulse.cpp \
+    $(PULSE_PATH)/sprites/Obstacle.cpp \
+    $(PULSE_PATH)/sprites/SpritePhysicsBody.cpp \
+    $(PULSE_PATH)/sprites/ObstaclePhysicsBody.cpp \
+    $(PULSE_PATH)/ui/MenuSlider.cpp \
+    $(PULSE_PATH)/generators/ObstacleGenerator.cpp
 
-LOCAL_C_INCLUDES := $(ROOT_PATH)/src \
-                    $(ROOT_PATH)/libs/cpputils/include \
-                    $(ROOT_PATH)/libs/GSL/include \
-                    $(ROOT_PATH)/libs/range-v3/include
-
-# _COCOS_HEADER_ANDROID_BEGIN
-# _COCOS_HEADER_ANDROID_END
+LOCAL_C_INCLUDES := \
+	$(INC_PATH) \
+	$(LIB_PATH)/cpputils/include \
+	$(LIB_PATH)/GSL/include \
+	$(LIB_PATH)/range-v3/include
 
 LOCAL_STATIC_LIBRARIES := cocos2dx_static
-
-# _COCOS_LIB_ANDROID_BEGIN
-# _COCOS_LIB_ANDROID_END
 
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,.)
-
-# _COCOS_LIB_IMPORT_ANDROID_BEGIN
-# _COCOS_LIB_IMPORT_ANDROID_END
