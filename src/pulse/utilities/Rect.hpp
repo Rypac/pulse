074e77598d31ca86922082e3fc17299afdd34e8d
Ryan Davis <ryan.davis.dev@gmail.com>
2016-04-25 16:51:54 +1000

    Find intersection of two rects

--- src/pulse/utilities/Rect.hpp
+++ src/pulse/utilities/Rect.hpp
@@ -31,4 +31,20 @@ static inline bool withinBounds(const Rect& rect, const Rect& bounds) {
     return withinHorizontalBounds(rect, bounds) && withinVerticalBounds(rect, bounds);
 }
 
+static inline bool intersects(const Rect& rect1, const Rect& rect2) {
+    return rect1.getMinX() < rect2.getMaxX() && rect1.getMaxX() > rect2.getMinX() &&
+        rect1.getMinY() < rect2.getMaxY() && rect1.getMaxY() > rect2.getMinY();
+}
+
+static inline Rect intersection(const Rect& rect1, const Rect& rect2) {
+    if (!intersects(rect1, rect2)) {
+        return Rect::ZERO;
+    }
+    const auto x = std::max(rect1.getMinX(), rect2.getMinX());
+    const auto y = std::max(rect1.getMinY(), rect2.getMinY());
+    const auto width = std::min(rect1.getMaxX(), rect2.getMaxX()) - x;
+    const auto height = std::min(rect1.getMaxY(), rect2.getMaxY()) - y;
+    return Rect{x, y, width, height};
+}
+
 } }
