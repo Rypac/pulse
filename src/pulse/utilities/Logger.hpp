42a52ea68aa27cad591b1648bde23d059ca6ada0
Ryan Davis <ryan.davis.dev@gmail.com>
2016-04-25 15:37:17 +1000

    Use unified interface for logger

--- src/pulse/utilities/Logger.hpp
+++ src/pulse/utilities/Logger.hpp
@@ -3,19 +3,19 @@
 #include "cocos2d.h"
 
 namespace pulse {
-namespace log {
+namespace logger {
 
-void vec(const cocos2d::Vec2& vec) {
+void log(const cocos2d::Vec2& vec) {
     cocos2d::log("x = %f, y = %f", vec.x, vec.y);
 }
 
-void size(const cocos2d::Size& size) {
+void log(const cocos2d::Size& size) {
     cocos2d::log("width = %f, height = %f", size.width, size.height);
 }
 
-void rect(const cocos2d::Rect& rect) {
-    log::vec(rect.origin);
-    log::size(rect.size);
+void log(const cocos2d::Rect& rect) {
+    logger::log(rect.origin);
+    logger::log(rect.size);
 }
 
 } }
