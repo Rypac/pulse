b52250fc60842e2d93ad71a6af70c14fc484d845
Ryan Davis <ryan.davis.dev@gmail.com>
2016-04-28 20:51:06 +1000

    Increase default player slow motion time scale

--- src/pulse/models/GameOptions.hpp
+++ src/pulse/models/GameOptions.hpp
@@ -9,7 +9,7 @@ struct GameOptions {
     cocos2d::Vec2 accelerometerSensitivity{2200, 1500};
     float obstacleFrequency{1};
     float obstacleSpeed{3.5};
-    TimeScale slowMotionTimeScale{0.4, 0.3};
+    TimeScale slowMotionTimeScale{1.0, 0.3};
 };
 
 }
