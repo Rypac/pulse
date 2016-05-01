cad6ca369a42348073e243420906d8775d353a61
Ryan Davis <ryan.davis.dev@gmail.com>
2016-05-01 12:32:29 +1000

    Enable immersive fullscreen mode on Android

--- proj.android-studio/app/src/org/cocos2dx/cpp/AppActivity.java
+++ proj.android-studio/app/src/org/cocos2dx/cpp/AppActivity.java
@@ -1,29 +1,34 @@
-/****************************************************************************
-Copyright (c) 2015 Chukong Technologies Inc.
- 
-http://www.cocos2d-x.org
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
 package org.cocos2dx.cpp;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
+import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
 
 public class AppActivity extends Cocos2dxActivity {
+
+    private Cocos2dxGLSurfaceView glSurfaceView;
+
+    public Cocos2dxGLSurfaceView onCreateView() {
+        glSurfaceView = new Cocos2dxGLSurfaceView(this);
+        glSurfaceView.setEGLConfigChooser(5, 6, 5, 0, 16, 8);
+        this.hideSystemUI();
+        return glSurfaceView;
+    }
+
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            this.hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        glSurfaceView.setSystemUiVisibility(
+            Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+            Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+            Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+            Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+            Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_FULLSCREEN |
+            Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+    }
 }
