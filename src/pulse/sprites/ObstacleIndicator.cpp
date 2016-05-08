f192605e18d4c85fd9a7644b50da45e3ba4145a7
Ryan Davis <ryan.davis.dev@gmail.com>
2016-05-08 12:28:28 +1000

    Add obstacle indicator particle effects and position in scene based on direction

--- src/pulse/sprites/ObstacleIndicator.cpp
+++ src/pulse/sprites/ObstacleIndicator.cpp
@@ -1,14 +1,53 @@
 #include "pulse/sprites/ObstacleIndicator.hpp"
+#include "pulse/utilities/Geometry.hpp"
 
 using namespace cocos2d;
 using namespace pulse;
 
-ObstacleIndicator* ObstacleIndicator::create() {
-    ObstacleIndicator *column = new (std::nothrow) ObstacleIndicator();
-    if (column && column->init()) {
+ObstacleIndicator* ObstacleIndicator::create(Direction direction, const Rect& frame) {
+    ObstacleIndicator *column = new (std::nothrow) ObstacleIndicator{direction};
+    if (column && column->initWithFrame(frame)) {
         column->autorelease();
         return column;
     }
     delete column;
     return nullptr;
+}
+
+bool ObstacleIndicator::initWithFrame(const Rect& frame) {
+    if (!ParticleSystemQuad::initWithFile(particleFilePath())) {
+        return false;
+    }
+
+    setContentSize(indicatorSize(frame));
+    setPosition(startingPosition(frame));
+
+    return true;
+}
+
+std::string ObstacleIndicator::particleFilePath() const {
+    switch (direction) {
+        case Direction::North: return "particles/obstacle_indicator_bottom.plist";
+        case Direction::South: return "particles/obstacle_indicator_top.plist";
+        case Direction::East: return "particles/obstacle_indicator_left.plist";
+        case Direction::West: return "particles/obstacle_indicator_right.plist";
+    }
+}
+
+Size ObstacleIndicator::indicatorSize(const Rect& frame) const {
+    switch (direction) {
+        case Direction::North:
+        case Direction::South: return Size{frame.size.width, thickness};
+        case Direction::East:
+        case Direction::West: return Size{thickness, frame.size.height};
+    }
+}
+
+Vec2 ObstacleIndicator::startingPosition(const Rect& frame) const {
+    switch (direction) {
+        case Direction::North: return geometry::bottomCenterOf(frame);
+        case Direction::South: return geometry::topCenterOf(frame);
+        case Direction::East: return geometry::centerLeftOf(frame);
+        case Direction::West: return geometry::centerRightOf(frame);
+    }
 }
--- src/pulse/sprites/ObstacleIndicator.hpp
+++ src/pulse/sprites/ObstacleIndicator.hpp
@@ -1,15 +1,25 @@
 #pragma once
 
 #include "cocos2d.h"
+#include "pulse/utilities/Direction.hpp"
 
 namespace pulse {
 
-class ObstacleIndicator : public cocos2d::Sprite {
+class ObstacleIndicator : public cocos2d::ParticleSystemQuad {
   public:
-    static ObstacleIndicator* create();
+    static ObstacleIndicator* create(Direction direction, const cocos2d::Rect& frame);
+
+    cocos2d::Vec2 startingPosition(const cocos2d::Rect& frame) const;
+    cocos2d::Size indicatorSize(const cocos2d::Rect& frame) const;
+    std::string particleFilePath() const;
 
   protected:
-    virtual bool init() override;
+    ObstacleIndicator(Direction direction): direction{direction} {}
+    virtual bool initWithFrame(const cocos2d::Rect& frame);
+
+  private:
+    const Direction direction;
+    const float thickness{80};
 };
 
 }
