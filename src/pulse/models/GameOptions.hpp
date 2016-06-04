b8ecfa86e4eb76dccc4534503664d7fa17608a65
Ryan Davis <ryan.davis.dev@gmail.com>
2016-06-04 16:32:54 +1000

    Add mode to game options

--- src/pulse/models/GameOptions.hpp
+++ src/pulse/models/GameOptions.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "cocos2d.h"
+#include "pulse/models/GameMode.hpp"
 #include "pulse/models/TimeScale.hpp"
 
 namespace pulse {
@@ -8,6 +9,7 @@ namespace pulse {
 struct GameOptions {
     float obstacleFrequency{1};
     float obstacleSpeed{3.5};
+    GameMode gameMode{GameMode::Classic};
     TimeScale slowMotionTimeScale{1.0, 0.3};
 };
 
