5d7837c867f59904583cfb424dd3aef9dda021e6
Ryan Davis <ryan.davis.dev@gmail.com>
2016-06-12 13:01:38 +1000

    Ensure objects are valid after initialisation

--- src/pulse/extensions/Ref.hpp
+++ src/pulse/extensions/Ref.hpp
@@ -4,15 +4,24 @@
 #include <type_traits>
 
 template <typename T, typename... Args, typename = typename std::enable_if<std::is_base_of<cocos2d::Ref, T>::value>::type>
+inline T* owned(Args&&... args) {
+    return new (std::nothrow) T(std::forward<Args>(args)...);
+}
+
+template <typename T, typename... Args, typename = typename std::enable_if<std::is_base_of<cocos2d::Ref, T>::value>::type>
 inline T* autoreleased(Args&&... args) {
-    auto object = new (std::nothrow) T(std::forward<Args>(args)...);
-    object->autorelease();
+    auto object = owned<T>(std::forward<Args>(args)...);
+    if (object) {
+        object->autorelease();
+    }
     return object;
 }
 
 template <typename T, typename... Args, typename = typename std::enable_if<std::is_base_of<cocos2d::Ref, T>::value>::type>
 inline T* retained(Args&&... args) {
-    auto object = new (std::nothrow) T(std::forward<Args>(args)...);
-    object->retain();
+    auto object = autoreleased<T>(std::forward<Args>(args)...);
+    if (object) {
+        object->retain();
+    }
     return object;
 }
