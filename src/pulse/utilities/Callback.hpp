8845bc1722390dcaf7a5fcd4b617bb56932ebeb9
Ryan Davis <ryan.davis.dev@gmail.com>
2016-06-25 12:19:57 +1000

    Add universal reference overload to member_callback function

--- src/pulse/utilities/Callback.hpp
+++ src/pulse/utilities/Callback.hpp
@@ -10,8 +10,15 @@ inline void safe_callback(Fn callback, Args&&... args) {
 }
 
 template <typename T, typename Fn>
-inline auto member_callback(T obj, Fn func) {
+inline auto member_callback(T* obj, Fn func) {
     return [obj, func](auto&&... args) {
-        return ((obj)->*(func))(std::forward<decltype(args)>(args)...);
+        return (obj->*func)(std::forward<decltype(args)>(args)...);
+    };
+}
+
+template <typename T, typename Fn>
+inline auto member_callback(T&& obj, Fn func) {
+    return [obj = std::forward<T>(obj), func](auto&&... args) mutable {
+        return (obj.*func)(std::forward<decltype(args)>(args)...);
     };
 }
