e8a4e50013c19ff65d189e40a0f32d12a389e3e5
Ryan Davis <ryan.davis.dev@gmail.com>
2016-06-25 12:31:02 +1000

    Use enable_if_t helper for template requirements

--- src/pulse/extensions/Ref.hpp
+++ src/pulse/extensions/Ref.hpp
@@ -3,12 +3,12 @@
 #include "cocos2d.h"
 #include <type_traits>
 
-template <typename T, typename... Args, typename = typename std::enable_if<std::is_base_of<cocos2d::Ref, T>::value>::type>
+template <typename T, typename... Args, typename = std::enable_if_t<std::is_base_of<cocos2d::Ref, T>::value>>
 inline T* owned(Args&&... args) {
     return new (std::nothrow) T(std::forward<Args>(args)...);
 }
 
-template <typename T, typename... Args, typename = typename std::enable_if<std::is_base_of<cocos2d::Ref, T>::value>::type>
+template <typename T, typename... Args, typename = std::enable_if_t<std::is_base_of<cocos2d::Ref, T>::value>>
 inline T* autoreleased(Args&&... args) {
     auto object = owned<T>(std::forward<Args>(args)...);
     if (object) {
@@ -17,7 +17,7 @@ inline T* autoreleased(Args&&... args) {
     return object;
 }
 
-template <typename T, typename... Args, typename = typename std::enable_if<std::is_base_of<cocos2d::Ref, T>::value>::type>
+template <typename T, typename... Args, typename = std::enable_if_t<std::is_base_of<cocos2d::Ref, T>::value>>
 inline T* retained(Args&&... args) {
     auto object = autoreleased<T>(std::forward<Args>(args)...);
     if (object) {
