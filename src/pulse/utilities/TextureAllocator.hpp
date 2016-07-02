977c56e5faeb3c26bbc944316f06d1aaf73ccc16
Ryan Davis <ryan.davis.dev@gmail.com>
2016-07-02 17:50:57 +1000

    Allocate image resources asynchronously

--- src/pulse/utilities/TextureAllocator.hpp
+++ src/pulse/utilities/TextureAllocator.hpp
@@ -14,19 +14,19 @@ struct TextureAllocator {
 
     static void initialiseDigits(cocos2d::TextureCache* textureCache) {
         for (auto&& digit : Resources::Images::Score::Digits) {
-            textureCache->addImage(digit);
+            textureCache->addImageAsync(digit, nullptr);
         }
     }
 
     static void initialiseButtons(cocos2d::TextureCache* textureCache) {
-        textureCache->addImage(Resources::Buttons::Achievements);
-        textureCache->addImage(Resources::Buttons::Blank);
-        textureCache->addImage(Resources::Buttons::Home);
-        textureCache->addImage(Resources::Buttons::Modes);
-        textureCache->addImage(Resources::Buttons::Pause);
-        textureCache->addImage(Resources::Buttons::Play);
-        textureCache->addImage(Resources::Buttons::Reset);
-        textureCache->addImage(Resources::Buttons::Restart);
+        textureCache->addImageAsync(Resources::Buttons::Achievements, nullptr);
+        textureCache->addImageAsync(Resources::Buttons::Blank, nullptr);
+        textureCache->addImageAsync(Resources::Buttons::Home, nullptr);
+        textureCache->addImageAsync(Resources::Buttons::Modes, nullptr);
+        textureCache->addImageAsync(Resources::Buttons::Pause, nullptr);
+        textureCache->addImageAsync(Resources::Buttons::Play, nullptr);
+        textureCache->addImageAsync(Resources::Buttons::Reset, nullptr);
+        textureCache->addImageAsync(Resources::Buttons::Restart, nullptr);
     }
 };
 
